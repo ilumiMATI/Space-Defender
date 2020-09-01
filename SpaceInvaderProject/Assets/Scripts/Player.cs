d0d57472c4bc93ddd513aa471d354e8a6a704c83
Mateusz Antczak <mati98848@gmail.com>
2020-09-01 05:51:02 +0200

    Clamped player movement

--- SpaceInvaderProject/Assets/Scripts/Player.cs
+++ SpaceInvaderProject/Assets/Scripts/Player.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 8f;
+    [SerializeField] float padding = 1f;
 
-    // cache
-    Camera theCamera;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        theCamera = FindObjectOfType<Camera>();
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -24,18 +36,20 @@ public class Player : MonoBehaviour
 
     private void RapidMove()
     {
-        var deltaX = Input.GetAxisRaw("Horizontal");
-        var newXPos = transform.position.x + deltaX * Time.deltaTime * moveSpeed;
-        var deltaY = Input.GetAxisRaw("Vertical");
-        var newYPos = transform.position.y + deltaY * Time.deltaTime * moveSpeed;
+        var deltaX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
     private void SmoothMove()
     {
-        var deltaX = Input.GetAxis("Horizontal");
-        var newXPos = transform.position.x + deltaX * Time.deltaTime * moveSpeed;
-        var deltaY = Input.GetAxis("Vertical");
-        var newYPos = transform.position.y + deltaY * Time.deltaTime * moveSpeed;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 
@@ -43,11 +57,15 @@ public class Player : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-            var touchPos = Input.GetTouch(0).position;
-            touchPos = theCamera.ScreenToWorldPoint(touchPos);
-            Vector2 deltaPos = touchPos - (Vector2)transform.position;
-            deltaPos.Normalize();
-            Vector2 newPos = (Vector2)transform.position + deltaPos * Time.deltaTime * moveSpeed;
+            var touchScreenPos = Input.GetTouch(0).position;
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touchScreenPos);
+            Vector2 deltaPos = (touchPos - (Vector2)transform.position).normalized * Time.deltaTime * moveSpeed;
+
+            Vector2 newPos = (Vector2)transform.position + deltaPos;
+
+            newPos.x = Mathf.Clamp(newPos.x, xMin, xMax);
+            newPos.y = Mathf.Clamp(newPos.y, yMin, yMax);
+
             transform.position = newPos;
         }
     }
