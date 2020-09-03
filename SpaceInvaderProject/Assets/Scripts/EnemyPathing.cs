cddb829c96fcdd5cfc16c90d1846427c5f32bdd8
Mateusz Antczak <mati98848@gmail.com>
2020-09-03 07:59:34 +0200

    Added basic pathing to Enemy

---
+++ SpaceInvaderProject/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            var targetPos = waypoints[waypointIndex].position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movementThisFrame);
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
