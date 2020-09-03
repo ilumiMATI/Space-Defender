a317fee9d90745927a562b03a23ebd8c787a6eff
Mateusz Antczak <mati98848@gmail.com>
2020-09-03 15:24:36 +0200

    Added Enemy wave config
    created wave 1 based on this wave scriptable object

---
+++ SpaceInvaderProject/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.2f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetPathPrefab() { return pathPrefab; }
+    public GameObject GetEnemyPrefab() { return enemyPrefab; }
+    public float GetTimeBetweenSpawns() { return timeBetweenSpawns; }
+    public float GetSpawnRandomFactor() { return spawnRandomFactor; }
+    public int GetNumberOfEnemies() { return numberOfEnemies; }
+    public float GetMoveSpeed() { return moveSpeed; }
+}
