1195db096a526bde6801e3befd075f0d38f8e03d
Mateusz Antczak <mati98848@gmail.com>
2020-09-06 20:11:14 +0200

    StartMenu added

---
+++ SpaceInvaderProject/Assets/Scripts/DoNotDestroyOnLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroyOnLoad : MonoBehaviour
+{
+    void Awake()
+    {
+        int elementsCount = FindObjectsOfType<DoNotDestroyOnLoad>().Length;
+        if (elementsCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ SpaceInvaderProject/Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
